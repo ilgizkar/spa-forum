a795e9a2efc8efc49a448c1fef56f1f1f7b0b8b6
Ilgiz <karabaev.il@yandex.ru>
2020-05-05 21:19:03 +0300

    Make relationships

--- app/Model/Question.php
+++ app/Model/Question.php
@@ -2,9 +2,23 @@
 
 namespace App\Model;
 
+use App\User;
 use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
 {
-    //
+    public function user()
+    {
+        $this->belongsTo(User::class);
+    }
+
+    public function replies()
+    {
+        $this->hasMany(Reply::class);
+    }
+
+    public function category()
+    {
+        $this->belongsTo(Category::class);
+    }
 }
--- app/Model/Reply.php
+++ app/Model/Reply.php
@@ -2,9 +2,23 @@
 
 namespace App\Model;
 
+use App\User;
 use Illuminate\Database\Eloquent\Model;
 
 class Reply extends Model
 {
-    //
+    public function user()
+    {
+        $this->belongsTo(User::class);
+    }
+
+    public function question()
+    {
+        $this->belongsTo(Question::class);
+    }
+
+    public function likes()
+    {
+        $this->hasMany(Like::class);
+    }
 }
