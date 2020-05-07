97540e637a0c32d5437b5e2a374427372d2eee5f
Ilgiz <karabaev.il@yandex.ru>
2020-05-08 02:47:27 +0300

    Make vue rotes and vuetify

---
+++ resources/js/components/Toolbar.vue
@@ -0,0 +1,23 @@
+<template>
+    <v-toolbar>
+<!--    <v-app-bar-nav-icon></v-app-bar-nav-icon>-->
+        <v-toolbar-title>SPA Forum</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-toolbar-items class="hidden-sm-and-down">
+            <v-btn text small color="primary">Форум</v-btn>
+            <v-btn text small color="primary">Задать вопрос</v-btn>
+            <v-btn text small color="primary">Категории</v-btn>
+            <v-btn text small color="primary">Логин</v-btn>
+        </v-toolbar-items>
+    </v-toolbar>
+</template>
+
+<script>
+    export default {
+        name: "Toolbar"
+    }
+</script>
+
+<style scoped>
+
+</style>
